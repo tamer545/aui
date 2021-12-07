5f30c13f46d64a17424964907074f0886e9e09e2
Tamer545 <8235.Lohn>
2021-12-07 20:51:15 +0100

    feat: add popup & fix table

--- src/App.js
+++ src/App.js
@@ -1,101 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
 import {
     Button,
     Container,
     Paper,
-    Table, TableBody,
+    Table,
+    TableBody,
     TableCell,
     TableContainer,
-    TableHead, TablePagination,
+    TableHead,
+    TablePagination,
     TableRow,
-    TextField
+    TextField,
+    Tooltip
 } from "@mui/material";
-import {useState} from "react";
-import {DatePicker, LocalizationProvider} from "@mui/lab";
-import DateAdapter from '@mui/lab/AdapterDateFns';
-
-const columns = [
-    {id: 'name', label: 'Name', minWidth: 170},
-    {id: 'dateAdded', label: 'Date Added', minWidth: 100},
-    {
-        id: 'dateToDo',
-        label: 'Date To Do',
-        minWidth: 170,
-        align: 'right',
-    },
-];
-
-function addRow(name, dateAdded, dateToDo) {
-    rows.push({name, dateAdded, dateToDo})
-}
-
-let rows = []
-
-const TableComponent = () => {
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(10);
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
-
-    return (
-        <Paper sx={{width: '100%', overflow: 'hidden'}}>
-            <TableContainer sx={{maxHeight: 440}}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{minWidth: column.minWidth}}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow hover role="checkbox" tabIndex={-1}>
-                                    {columns.map((column, index) => {
-                                        const value = row[column.id];
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
-    )
-}
+import {useEffect, useState} from "react";
+import PopoverPopupState from "./PopUp";
 
 function App() {
+    const columns = [
+        {id: 'name', label: 'Name', minWidth: 170},
+        {id: 'dateAdded', label: 'Date Added', minWidth: 100},
+        {
+            id: 'dateToDo',
+            label: 'Date To Do',
+            minWidth: 170,
+            align: 'right',
+        },
+    ];
+
+    const [rows, setRows] = useState([{}])
     const [taskName, setTaskName] = useState('')
     const [dateToDo, setDateToDo] = useState('')
+    const [extraInfo, setExtraInfo] = useState('(no info)')
+    const [allInfos, setAllInfos] = useState([])
+
+    function addRow(name, dateAdded, dateToDo) {
+        setRows([...rows, {name, dateAdded, dateToDo}])
+        setAllInfos([...allInfos, extraInfo])
+    }
+
+    const TableComponent = () => {
+        const [page, setPage] = useState(0);
+        const [rowsPerPage, setRowsPerPage] = useState(10);
+
+        const handleChangePage = (event, newPage) => {
+            setPage(newPage);
+        };
+
+        const handleChangeRowsPerPage = (event) => {
+            setRowsPerPage(+event.target.value);
+            setPage(0);
+        };
+
+        return (
+            <Paper sx={{width: '100%', overflow: 'hidden'}}>
+                <TableContainer sx={{maxHeight: 440}}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{minWidth: column.minWidth}}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, idx) => {
+                                return (
+                                    <PopoverPopupState>
+                                        <Tooltip title={allInfos[idx]}>
+                                            <TableRow hover role="checkbox" tabIndex={-1}>
+                                                {columns.map((column, index) => {
+                                                    const value = row[column.id];
+                                                    return (
+                                                        <TableCell key={column.id} align={column.align}>
+                                                            {value}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                            </TableRow>
+                                        </Tooltip>
+                                    </PopoverPopupState>
+                                );
+                            })}
+                        </TableBody>
+
+
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        )
+    }
+
+    useEffect(() => {
+        setAllInfos([...allInfos, ''])
+    }, [])
+
     return (
         <Container>
             <h1>AUI</h1>
@@ -104,10 +118,16 @@ function App() {
             <br/><br/>
             <TextField id="outlined-basic" label="Task Name" variant="outlined" value={taskName}
                        onChange={e => setTaskName(e.target.value)}/>
-            <TextField id="outlined-basic" label="Task Name" variant="outlined" value={dateToDo}
-                       onChange={e => setDateToDo(e.target.value)}/>
+            <TextField id="outlined-basic" label="Extra Information" variant="outlined" value={extraInfo}
+                       onChange={e => setExtraInfo(e.target.value)}/>
+            <TextField
+                label="Date To Do"
+                type="date"
+                value={dateToDo}
+                onChange={e => setDateToDo(e.target.value)}
+            />
             <Button variant="contained"
-                    onClick={() => addRow(taskName, new Date().toLocaleDateString("uk-Uk"), '93')}>Add</Button>
+                    onClick={() => addRow(taskName, new Date().toLocaleDateString("uk-Uk"), new Date(dateToDo).toLocaleDateString("uk-UK"))}>Add</Button>
             <br/><br/>
             <TableComponent/>
         </Container>
---
+++ src/PopUp.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
+import PopupState, {bindTrigger, bindPopover} from 'material-ui-popup-state';
+
+export default function PopoverPopupState(props) {
+    return (
+        <PopupState variant="popover">
+            {(popupState) => (
+                <div>
+                    <Button variant="contained" {...bindTrigger(popupState)}>
+                        Open Popover
+                    </Button>
+                    <Popover
+                        {...bindPopover(popupState)}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'center',
+                        }}
+                    >
+                        <Typography sx={{p: 2}}>{props.content}</Typography>
+                    </Popover>
+                </div>
+            )}
+        </PopupState>
+    );
+}
